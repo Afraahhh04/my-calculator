ae34b02a15195830e02b5276af3ea252c8894e07
afraahhh04 <afraahfatima04@gmail.com>
2024-04-21 16:19:26 +0530

    added orange color

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function calculate() {
     var result = eval(expression);
     input.textContent = result;
 }
+function clearDisplay() {
+    document.getElementById('input').innerText = '0'; // Set display to 0
+}
+
+function deleteLast() {
+    let display = document.getElementById('input').innerText;
+    if (display.length > 1) {
+        document.getElementById('input').innerText = display.slice(0, -1); // Remove the last character
+    } else {
+        document.getElementById('input').innerText = '0'; // If there's only one character, set display to 0
+    }
+}
